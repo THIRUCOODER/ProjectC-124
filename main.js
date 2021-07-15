9def95c0eb9e5268cd9f3a4cd86c5322c9a5661d
THIRUCOODER <78685006+THIRUCOODER@users.noreply.github.com>
2021-07-15 18:02:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(750,700);
+    
+    canvas = createCanvas(750,750);
+    canvas.position(760,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+body {
+    background: linear-gradient(#5FF2C1, white);
+}
+function modelLoaded() {
+ console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    background('#00FA9A');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
